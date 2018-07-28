e317591f487265452de2da56c636fedc2404ea63
BornWinter <a_lex92@mail.ru>
2018-07-28 19:27:19 +0400

    Add project files.

---
+++ firstApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.Add(2, 3);
+         
+        }
+        class Calculator
+        {
+            public static void Add (int x, int y)
+            {
+                int z = x + y;
+                Console.WriteLine($"Сумма {x} + {y} = {z}");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
